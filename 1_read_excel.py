82322cdc408e6462ca03d4c071ce2a75874568d9
KABILESH M <kabileshclg0678@gmail.com>
2025-12-04 21:12:49 +0530

    Update 1_read_excel.py

--- 1_read_excel.py
+++ 1_read_excel.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = r"D:\app\Excel\fitpulse.xlsx"
+file_path = r"D:\fitpulse_health\Excel\fitpulse.xlsx"
 st.title("FitPulse Data")
 # Read Excel file
 data = pd.read_excel(file_path)
